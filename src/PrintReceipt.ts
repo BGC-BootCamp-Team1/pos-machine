2146b7a64b2e7039cfefdfca7c7bdc913b0695d0
tangqi <qtang5@slb.com>
2024-10-29 19:52:35 +0800

    Fix: update big structure

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -2,10 +2,11 @@ import {loadAllItems, loadPromotions} from './Dependencies'
 import { Item, Promotion, ReceiptItem } from './Model'
 
 export function printReceipt(tags: string[]): string {
-  let allItems:Item[] = loadAllItems();
-  let allPromotions:Promotion[] =loadPromotions();
-  let allReceipts:ReceiptItem[] = genAllReceipt(allItems,allPromotions);
-  let renderedReceipts:string = render(allReceipts);
+  let allItems: Item[] = loadAllItems();
+  let allPromotions: Promotion[] = loadPromotions();
+  let allReceipts = genAllReceipt(allItems, tags)
+  let discountedReceipts: ReceiptItem[] = checkPromotion(allReceipts, allPromotions);
+  let renderedReceipts: string = render(discountedReceipts);
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
@@ -15,12 +16,15 @@ Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
 }
-export function genAllReceipt(allItems:Item[], allPromotions:Promotion[]): ReceiptItem[]{
-  
+export function genAllReceipt(allItems: Item[], tags: string[]): Map<string, ReceiptItem> {
+  const itemMap = new Map<string, ReceiptItem>();
+  return itemMap
+}
+
+function checkPromotion(itemMap: Map<string, ReceiptItem>, allPromotions: Promotion[]): ReceiptItem[] {
   return []
-} 
+}
 
 function render(allReceipts: ReceiptItem[]): string {
-
   return ''
 }
