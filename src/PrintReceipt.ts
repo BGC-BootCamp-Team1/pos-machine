75747c00f91a972b748d61d23272b1909a836ae9
tangqi <qtang5@slb.com>
2024-10-29 20:40:30 +0800

    Fix: remove useless comments

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -11,30 +11,26 @@ export function printReceipt(tags: string[]): string {
 }
 export function genAllReceipt(allItems: Item[], tags: string[]): Map<string, ReceiptItem> {
 
-  // 创建一个Map来统计每个商品的数量
   const itemMap = new Map<string, ReceiptItem>();
 
-  // 解析tags，统计每个商品的数量
   tags.forEach(tag => {
     const [barcode, quantityStr] = tag.split('-');
     const quantity = quantityStr ? parseFloat(quantityStr) : 1;
 
     if (itemMap.has(barcode)) {
-      // 如果已经存在此商品，增加数量
       const receiptItem = itemMap.get(barcode)!;
       receiptItem.quantity += quantity;
-      receiptItem.subtotal = receiptItem.quantity * receiptItem.unitMoney; // 更新小计
-    } else {
-      // 找到该商品的详细信息
+      receiptItem.subtotal = receiptItem.quantity * receiptItem.unitMoney; 
+    } 
+    else {
       const item = allItems.find(item => item.barcode === barcode);
       if (item) {
-        // 如果是新商品，创建新的 ReceiptItem 并添加到 Map
         const newReceiptItem: ReceiptItem = {
           name: item.name,
           quantity: quantity,
           unitName: item.unit,
           unitMoney: item.price,
-          subtotal: quantity * item.price // 初始小计为数量乘以单价
+          subtotal: quantity * item.price 
         };
         itemMap.set(barcode, newReceiptItem);
       }
@@ -46,7 +42,6 @@ export function genAllReceipt(allItems: Item[], tags: string[]): Map<string, Rec
 }
 
 export function checkPromotion(itemMap: Map<string, ReceiptItem>, allPromotions: Promotion[]): ReceiptItem[] {
-  // 检查促销并调整小计
   for (const promotion of allPromotions) {
     if (promotion.type === 'BUY_TWO_GET_ONE_FREE') {
       promotion.barcodes.forEach(barcode => {
