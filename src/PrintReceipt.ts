16f3eaccae1e1121beb32462f9bff524d638fa52
htian15 <htian15@slb.com>
2024-10-30 01:32:10 +0800

    feat: finish render and top printReceipt func

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,24 +1,31 @@
 // import { parseTags } from "./parseTags";
 import { loadAllItems, loadPromotions } from "./Dependencies";
+import { GetIProductList } from "./GetIProductList";
+import { GetItemQuantity } from "./GetItemQuantity";
 import { IProduct } from "./IProduct";
+import { UpdateIProductList } from "./UpdateIProductList";
 // import { getReciptContent } from './getReciptContent';
 
 export function printReceipt(tags: string[]): string {
   // const tradeProducts = parseTags(tags);
-  
-  let out: string = "";
+  const quantityMap = GetItemQuantity(tags);
+  const initialIProductList = GetIProductList(quantityMap);
+  const finalProducts = UpdateIProductList(initialIProductList);
+  let out: string = render(finalProducts);
   // out = render(tradeProducts.tradeProducts, tradeProducts.totalDiscount);
-  return out;
 
-  
+  return out;
 }
 
-function render(tradeProducts: IProduct[], discount: number): string {
+// function render(tradeProducts: IProduct[], discount: number): string {
+function render(finalProducts: IProduct[]): string {
   let totalPrice = 0;
+  let totalDiscount = 0;
   let receiptItems = "";
 
-  for (let item of tradeProducts) {
+  for (let item of finalProducts) {
     totalPrice += item.subtotal;
+    totalDiscount += item.subdiscount;
     receiptItems += `Name：${item.name}，Quantity：${item.quantity} ${
       item.unit
     }${item.quantity > 1 ? "s" : ""}，Unit：${item.price.toFixed(
@@ -26,9 +33,8 @@ function render(tradeProducts: IProduct[], discount: number): string {
     )}(yuan)，Subtotal：${item.subtotal.toFixed(2)}(yuan)\n`;
   }
 
- 
   let totalPriceStr = totalPrice.toFixed(2);
-  let totalDiscountStr = discount.toFixed(2);
+  let totalDiscountStr = totalDiscount.toFixed(2);
 
   return `***<store earning no money>Receipt ***
 ${receiptItems}----------------------
