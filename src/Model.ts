a3ca598c85670da179bbf78c7cfd16a23cfde4b2
tangqi <qtang5@slb.com>
2024-10-29 17:40:56 +0800

    Feat: implement interface Item and Receipt in Model

---
+++ src/Model.ts
@@ -0,0 +1,16 @@
+
+export interface Item
+{
+  barcode: string;
+  name: string;
+  unit: string;
+  price: number
+}
+export interface Receipt
+{
+    Name:string;
+    Quantity:number;
+    UnitName: string;
+    Unit: number;
+    Subtotal: number
+}
