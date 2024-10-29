f8afc627eb940ddc5e23914fd1ba75327cd364c5
Sheng Liu <SLiu40@slb.com>
2024-10-29 18:34:10 +0800

    FEAT.add calculateTotalPriceOneFree function

---
+++ src/Model.ts
@@ -0,0 +1,22 @@
+import {Decimal} from "decimal.js";
+
+export interface Item {
+    barcode: string;
+    name: string;
+    unit: string;
+    price: number;
+}
+
+
+export interface MetaData{
+    items:ItemMetaData[]
+    Total:Decimal;
+    DiscountedPrices:Decimal;
+}
+
+export interface ItemMetaData{
+    Name: string;
+    Quantity: Decimal;
+    Unit:number;
+    Subtotal:Decimal;
+}
--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,18 +1,19 @@
 import {loadAllItems, loadPromotions} from './Dependencies'
-import Decimal from 'decimal.js';
+import {Decimal} from 'decimal.js';
+import {MetaData, Item, ItemMetaData} from "./Model";
 
 function convert2Map(tags: string[]) {
-  let map = new Map<string,Decimal>();
+  let map = new Map<string,number>();
   for (let tag of tags) {
-    let quantity:Decimal;
+    let quantity:number = 0;
     let barcode:string = tag.split('-')[0];
     if(tag.split('-').length <= 1) {
-      quantity = new Decimal(1);
+      quantity = 1;
     }else {
-      quantity = new Decimal(tag.split('-')[1]);
+      quantity = Number( tag.split('-')[1] );
     }
     if(map.has(barcode)) {
-      map.set(barcode, quantity.add(map.get(barcode)!));
+      map.set(barcode, quantity+map.get(barcode)!);
     }else {
       map.set(barcode, quantity);
     }
@@ -20,29 +21,80 @@ function convert2Map(tags: string[]) {
   return map;
 }
 
-interface MetaData{
-  Name: string;
-  Quantity: number;
-  Unit:string;
-  Subtotal:Decimal;
-  Total:Decimal;
-  DiscountedPrices:Decimal;
+
+function generateReceiptMetaData(map: Map<string, number>, itemList:Item[], promotionList: { type: string; barcodes: string[] }[]):MetaData {
+  let metaData:MetaData={
+    DiscountedPrices: new Decimal(0),
+    Total: new Decimal(0),
+    items: []
+  }
+  let promotion = promotionList[0];
+
+  for (let [key, value] of map) {
+    let item :Item=itemList.find(obj => obj.barcode === key) as Item;
+
+    let itemMetaData:ItemMetaData = {
+      Name: item.name,
+      Quantity: new Decimal(value) ,
+      Unit:item.price,
+      Subtotal:new Decimal(0)
+    }
+
+    if(promotion.barcodes.includes(key)){
+      //disc
+      let subTotal= calculateTotalPriceOneFree(item.price,value);
+
+      itemMetaData.Subtotal = itemMetaData.Subtotal.add( subTotal);
+
+      metaData.DiscountedPrices = metaData.DiscountedPrices.add(item.price*value-subTotal);
+      metaData.Total =metaData.Total.add(subTotal);
+    }else {
+      itemMetaData.Subtotal = itemMetaData.Subtotal.add(item.price*value);
+
+      metaData.Total = metaData.Total.add(item.price*value);
+    }
+
+    metaData.items.push(itemMetaData)
+
+  }
+  return metaData;
 }
 
-function generateReceiptMetaData(map: Map<string, Decimal>, itemList:any[], promotionList: { type: string; barcodes: string[] }[]) {
-  for (let [key, value] of map) {
-    console.log(key, value);
+function render(metaData: MetaData) {
+  let receipt:string = "";
+  receipt+=`***<store earning no money>Receipt ***/n`
+  for(let item of metaData.items) {
+    receipt+=`Name：${item.Name}，Quantity：${item.Quantity} ${item.Unit}s，Unit：3.00(yuan)，Subtotal：12.00(yuan)
+`
+
+
   }
-
-
-  return [];
+  return "";
 }
 
 export function printReceipt(tags: string[]): string {
-  let itemList = loadAllItems();
+  let itemList = loadAllItems() as Item[];
   let promotionList = loadPromotions();
-  let map :Map<string,Decimal> =convert2Map(tags)
-  let metaDataList:MetaData[] = generateReceiptMetaData(map,itemList,promotionList);
-
-  return ''
+  let map :Map<string,number> =convert2Map(tags)
+  let metaData:MetaData = generateReceiptMetaData(map,itemList,promotionList);
+  let expectText:string = render(metaData);
+  return expectText
 }
+
+function calculateTotalPriceOneFree(price: number, quantity: number): number {
+  const actualQuantityToPay = Math.ceil(quantity / 3 * 2);
+  return actualQuantityToPay * price;
+}
+
+printReceipt(
+    [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ]
+)
