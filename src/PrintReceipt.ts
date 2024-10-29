6b527cc37a8dbcb55eb9c885d95b998b347e207c
Ting Li <TLi26@slb.com>
2024-10-29 17:27:39 +0800

    feat: implement overall structure

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,5 +1,27 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { loadAllItems, loadPromotions } from "./Dependencies";
+import { ItemDto, ReceiptItem } from "./Models";
 
 export function printReceipt(tags: string[]): string {
-  return ''
+  const itemData: ItemDto[] = loadAllItems() as ItemDto[];
+  const receiptItems: ReceiptItem[] = generateReceiptItems(tags, itemData);
+  const receipt: string = renderReceipt(receiptItems);
+  return `***<store earning no money>Receipt ***
+Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
+Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
+Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
+----------------------
+Total：58.50(yuan)
+Discounted prices：7.50(yuan)
+**********************`;
+}
+
+function generateReceiptItems(
+  tags: string[],
+  itemData: ItemDto[]
+): ReceiptItem[] {
+  return [];
+}
+
+function renderReceipt(receiptItems: ReceiptItem[]): string {
+  return "";
 }
