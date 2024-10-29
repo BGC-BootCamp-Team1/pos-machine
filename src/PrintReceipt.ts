416298c0dab812f4fbd4b6e5b93fc2680d8e9be0
tangqi <qtang5@slb.com>
2024-10-29 20:38:41 +0800

    Feat: implement printReceipt method

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -7,14 +7,7 @@ export function printReceipt(tags: string[]): string {
   let allReceipts = genAllReceipt(allItems, tags);
   let discountedReceipts: ReceiptItem[] = checkPromotion(allReceipts, allPromotions);
   let renderedReceipts: string = render(discountedReceipts);
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+  return renderedReceipts
 }
 export function genAllReceipt(allItems: Item[], tags: string[]): Map<string, ReceiptItem> {
 
